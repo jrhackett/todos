7466d975a09e25744a1b5314e7f6680ae8fc0ca8
Jacob Hackett <jacobrhackett@gmail.com>
2017-05-15 12:27:51 -0400

    throttling the saveState calls on the store subscription

--- src/index.js
+++ src/index.js
@@ -6,15 +6,16 @@ import { Provider } from 'react-redux'
 import App from './components/App'
 import reducer from './reducers'
 import { loadState, saveState } from './localStorage'
+import throttle from 'lodash/throttle'
 
 const persistedState = loadState()
 const store = createStore(reducer, persistedState)
 
-store.subscribe(() => {
+store.subscribe(throttle(() => {
   saveState({
     todos: store.getState().todos
   })
-})
+}, 1000))
 
 render(
   <Provider store={store}>
