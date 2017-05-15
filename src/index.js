10971da39d73826c13dce22832b922850a731f85
Jacob Hackett <jacobrhackett@gmail.com>
2017-05-15 14:08:28 -0400

    reducing the amount of work done by root index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { render } from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './components/App'
-import reducer from './reducers'
-import { loadState, saveState } from './localStorage'
-import throttle from 'lodash/throttle'
+import Root from './components/Root'
+import configureStore from './configureStore'
 
-const persistedState = loadState()
-const store = createStore(reducer, persistedState)
-
-store.subscribe(throttle(() => {
-  saveState({
-    todos: store.getState().todos
-  })
-}, 1000))
+const store = configureStore()
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Root store={store} />,
   document.getElementById('root')
 )
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Root store={store} />,
   document.getElementById('root')
 );
