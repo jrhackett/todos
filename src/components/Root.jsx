c0437e114aaa9a80eaafaa921a4ba0dcabf09918
Jacob Hackett <jacobrhackett@gmail.com>
2017-05-15 15:02:11 -0400

    reverting 4426ad53c1c2dc94992b3fe3648c930380bd679e

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import App from './App'
-import Home from './Home'
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/app" component={App} />
+        <Route exact path="/" component={App} />
       </Switch>
     </BrowserRouter>
   </Provider>
