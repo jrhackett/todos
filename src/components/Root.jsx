39f43ae374680335e9930fb799a362c0bb002e24
Jacob Hackett <jacobrhackett@gmail.com>
2017-05-15 14:39:12 -0400

    adding react router v4, looking to add details to each task eventually as well as users and this will help that

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import App from './App'
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={App} />
+      </Switch>
+    </BrowserRouter>
   </Provider>
 )
 
